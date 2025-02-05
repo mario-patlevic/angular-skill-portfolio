8b0866e92d09c57f82a9e32f81340c6ab86a17e2
Mario Patlevic <mpatlevic@yahoo.com>
2025-02-05 20:04:30 +0100

    feat: added icons to buttons

--- src/app/to-do-app/to-do-input/to-do-input.component.ts
+++ src/app/to-do-app/to-do-input/to-do-input.component.ts
@@ -1,14 +1,14 @@
 import { Component, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-to-do-input',
-  imports: [FormsModule],
+  imports: [FormsModule, MatIcon],
   templateUrl: './to-do-input.component.html',
-  styleUrl: './to-do-input.component.scss'
+  styleUrl: './to-do-input.component.scss',
 })
 export class ToDoInputComponent {
-
   todoInput = '';
 
   todoInputChange = output<string>();
--- src/app/to-do-app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-app/to-do-list/to-do-list.component.ts
@@ -1,7 +1,9 @@
 import { Component, input, output } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-to-do-list',
+  imports: [MatIconModule],
   templateUrl: './to-do-list.component.html',
   styleUrl: './to-do-list.component.scss',
 })
