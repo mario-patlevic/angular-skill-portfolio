07eee60c8e3fea7128c7adc5088a9c88ecd1f482
Mario Patlevic <mpatlevic@yahoo.com>
2025-02-05 19:25:39 +0100

    feat: improved functionality

--- src/app/to-do-app/to-do-app.component.ts
+++ src/app/to-do-app/to-do-app.component.ts
@@ -1,22 +1,32 @@
 import { Component } from '@angular/core';
-import { ToDoInputComponent } from "./to-do-input/to-do-input.component";
-import { ToDoListComponent } from "./to-do-list/to-do-list.component";
+import { ToDoInputComponent } from './to-do-input/to-do-input.component';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
+import { DefaultValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-to-do-app',
   imports: [ToDoInputComponent, ToDoListComponent],
   templateUrl: './to-do-app.component.html',
-  styleUrl: './to-do-app.component.scss'
+  styleUrl: './to-do-app.component.scss',
 })
 export class ToDoAppComponent {
-  todoList: string[] = ['Update for loop for to-do list array'];
-  
+  todoList: string[] = [
+    'Use local storage instead of array',
+    'Get complete button to work',
+    'Study and excercise coding',
+    'Go out',
+    'Excercise at gym',
+  ];
+
   title = 'To-do app';
 
   todoFromInput: string = '';
 
-  updateTaskLabel(value: string) {
+  addNewTask(value: string) {
     this.todoList.unshift(value);
-    console.log(this.todoList);
+  }
+
+  deleteTask(value: number) {
+    this.todoList.splice(value, 1);
   }
 }
